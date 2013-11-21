1ad40d3333dd16e17e9539e293af6673e48791dd
jhpy1024 <jhpy1024@gmail.com>
2013-11-21 19:41:16 +0000

    Added wall thingies.

--- Game.cpp
+++ Game.cpp
@@ -6,12 +6,43 @@
 #include "Ball.h"
 #include "Paddle.h"
 
+const float Game::WallSize = 20.f;
+
 Game::Game()
 : window_(sf::VideoMode(Game::Width, Game::Height), "Pong!")
 {
+	createEntities();
+	createWalls();
+}
+
+void Game::createWalls()
+{
+	centerWall_.setSize(sf::Vector2f(Game::WallSize, Game::Height));
+	centerWall_.setPosition(Game::Width / 2 - centerWall_.getSize().x / 2, 0.f);
+	centerWall_.setFillColor(sf::Color::Green);
+
+	bottomWall_.setSize(sf::Vector2f(Game::Width, Game::WallSize));
+	bottomWall_.setPosition(0.f, Game::Height - bottomWall_.getSize().y);
+	bottomWall_.setFillColor(sf::Color::Green);
+
+	topWall_.setSize(sf::Vector2f(Game::Width, Game::WallSize));
+	topWall_.setPosition(0.f, 0.f);
+	topWall_.setFillColor(sf::Color::Green);
+
+	leftWall_.setSize(sf::Vector2f(Game::WallSize, Game::Height));
+	leftWall_.setPosition(0.f, 0.f);
+	leftWall_.setFillColor(sf::Color::Green);
+
+	rightWall_.setSize(sf::Vector2f(Game::WallSize, Game::Height));
+	rightWall_.setPosition(Game::Width - rightWall_.getSize().x, 0.f);
+	rightWall_.setFillColor(sf::Color::Green);
+}
+
+void Game::createEntities()
+{
 	entities_.push_back(std::make_shared<Paddle>(
 		sf::Vector2f(15.f, 200.f),
-		sf::Vector2f(10.f, Game::Height / 2 - 200 / 2)));
+		sf::Vector2f(Game::WallSize, Game::Height / 2 - 200 / 2)));
 	entities_.push_back(std::make_shared<Ball>(10.f));
 }
 
@@ -40,6 +71,12 @@ void Game::update(sf::Time delta)
 void Game::render()
 {
 	window_.clear();
+
+	window_.draw(centerWall_);
+	window_.draw(bottomWall_);
+	window_.draw(topWall_);
+	window_.draw(leftWall_);
+	window_.draw(rightWall_);
 
 	for (auto entity : entities_)
 		entity->render(window_);
--- Game.h
+++ Game.h
@@ -21,11 +21,21 @@ public:
 
 	static const int Width = 640;
 	static const int Height = 480;
+	static const float WallSize;
 	
 private:
 	sf::RenderWindow window_;
 
+	sf::RectangleShape centerWall_;
+	sf::RectangleShape topWall_;
+	sf::RectangleShape bottomWall_;
+	sf::RectangleShape leftWall_;
+	sf::RectangleShape rightWall_;
+
 	std::vector<std::shared_ptr<Entity>> entities_;
+
+	void createEntities();
+	void createWalls();
 
 	const sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
 };
